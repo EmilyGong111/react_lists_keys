2af5ce5fe26377815e6778d2be13dc08d72dccb5
Emily <mei.gong@icloud.com>
2022-05-28 00:23:18 +1000

    feat: shopping list with static data

--- src/Content.js
+++ src/Content.js
@@ -1,56 +1,56 @@
-// import { useState } from 'react';
-// import { FaTrashAlt } from 'react-icons/fa';
+import { useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
 
 const Content = () => {
-//     const [items, setItems] = useState([
-//         {
-//             id: 1,
-//             checked: true,
-//             item: "One half pound bag of Cocoa Covered Almonds Unsalted"
-//         },
-//         {
-//             id: 2,
-//             checked: false,
-//             item: "Item 2"
-//         },
-//         {
-//             id: 3,
-//             checked: false,
-//             item: "Item 3"
-//         }
-//     ]);
-    const handleCheck1 = ()=>{
-        console.log('You clicked it')
-    }
-    const handleCheck2 = (name)=>{
-        console.log(`${name} clicked it`)
-    }
-    const handleCheck3 = (e)=>{
-        console.log(e)
-        console.log(e.target)
-        console.log(e.target.innerText)
-    }
-    // const handleCheck = (id) => {
-    //     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
-    //     setItems(listItems);
-    //     localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    const [items, setItems] = useState([
+        {
+            id: 1,
+            checked: true,
+            item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+        },
+        {
+            id: 2,
+            checked: false,
+            item: "Item 2"
+        },
+        {
+            id: 3,
+            checked: false,
+            item: "Item 3"
+        }
+    ]);
+    // const handleCheck1 = ()=>{
+    //     console.log('You clicked it')
     // }
+    // const handleCheck2 = (name)=>{
+    //     console.log(`${name} clicked it`)
+    // }
+    // const handleCheck3 = (e)=>{
+    //     console.log(e)
+    //     console.log(e.target)
+    //     console.log(e.target.innerText)
+    // }
+    const handleCheck = (id) => {
+        const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+        setItems(listItems);
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    }
 
-    // const handleDelete = (id) => {
-    //     const listItems = items.filter((item) => item.id !== id);
-    //     setItems(listItems);
-    //     localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-    // }
+    const handleDelete = (id) => {
+        const listItems = items.filter((item) => item.id !== id);
+        setItems(listItems);
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    }
 
     return (
         <main>
-            <button onClick={handleCheck1}>Click It</button>
+            {/* <button onClick={handleCheck1}>Click It</button>
             {/* function will be immediately called with the way below */}
-            <button onClick={handleCheck2('Emily')}>Click It</button> 
+            {/* <button onClick={handleCheck2('Emily')}>Click It</button>  */}
             {/* when you need to pass a parameter, you need a anonymous function to call the function you want to apply. As below */}
-            <button onClick={() => handleCheck2('Emily')}>Click It</button>
-            <button onClick={(e) => handleCheck3(e)}>Click It</button>
-{/* 
+            {/* <button onClick={() => handleCheck2('Emily')}>Click It</button> */}
+            {/* <button onClick={(e) => handleCheck3(e)}>Click It</button> */}
+
             {items.length ? (
                 <ul>
                     {items.map((item) => (
@@ -74,7 +74,7 @@ const Content = () => {
                 </ul>
             ) : (
                 <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
-            )} */}
+            )}
         </main>
     )
 }
